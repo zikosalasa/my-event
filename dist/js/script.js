986e062fcaa64bb9b7a9927f9e793790921bf3ee
omes <zikosalasa6@gmail.com>
2023-10-22 21:03:29 +0900

    my-event

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+//hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active'); 
+    navMenu.classList.toggle('hidden');
+});
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      screens: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
